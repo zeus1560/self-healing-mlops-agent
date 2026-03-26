1c3598506ad41cc4b136eec8373c6964a56a8a93
zeus1560 <asj2178@naver.com>
2026-03-26 15:03:21 +0900

    chore: remove venv folder to fix large file push errors
    Former-commit-id: 2365caed28c6bf1e775c85e83e9aab71019c2b60

--- venv/Lib/site-packages/torch/include/ATen/functorch/Macros.h
+++
@@ -1,8 +0,0 @@
-#if !defined(TORCH_STABLE_ONLY) && !defined(TORCH_TARGET_VERSION)
-#pragma once
-
-#define SINGLE_ARG(...) __VA_ARGS__
-
-#else
-#error "This file should not be included when either TORCH_STABLE_ONLY or TORCH_TARGET_VERSION is defined."
-#endif  // !defined(TORCH_STABLE_ONLY) && !defined(TORCH_TARGET_VERSION)
